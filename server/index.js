3ebd4a833b9fafe6938d904f4e619651f9d419a2
MAHESH SHRAVAN ROHANE <105151118+Mahesh77r@users.noreply.github.com>
2023-09-08 19:53:58 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ dotenv.config();
 app.use(cors(
   {
         origin: "https://crud-mern-client-pi.vercel.app",
-        methods:["POST","GET"],
+        methods:["POST","GET","PUT","DELETE"],
         credentials:true
     }
 ));
