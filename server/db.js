0da9b712418176df01386207fb1c8c7701236eb9
MAHESH SHRAVAN ROHANE <105151118+Mahesh77r@users.noreply.github.com>
2023-09-08 18:36:21 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const Connection = async(username,password) =>{
-    const URL =`mongodb+srv://${username}:${password}@cluster0.ek8sc.mongodb.net/SIHPROJECT?retryWrites=true&w=majority`;
+    const URL =`mongodb+srv://mahesh:Kln1iWjNOHrp29Ad@cluster0.ek8sc.mongodb.net/?retryWrites=true&w=majority`;
     try{
         await mongoose.connect(URL,{useUnifiedTopology: true, useNewUrlParser:true});
         console.log("Database Connected Successfully")
